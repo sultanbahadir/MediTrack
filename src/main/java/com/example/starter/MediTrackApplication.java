f4eaa437be9b4b90cb4bf69a7a959722972571fc
sultanbahadir <sultanb@gmail.com>
2026-07-15 14:25:10 +0300

    Initial commit

---
+++ src/main/java/com/example/starter/MediTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediTrackApplication.class, args);
+	}
+
+}
